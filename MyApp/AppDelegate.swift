3b8b8e9ada788118cfa4881143b0976113ec1954
cdhboy <cdhboy@163.com>
2018-07-18 15:30:19 +0800

    test

---
+++ MyApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MyApp
+//
+//  Created by chendh on 2018/7/18.
+//  Copyright © 2018年 my. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
